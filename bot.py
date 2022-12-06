fe9cfb927d1692df7749015b99ffb7d0fd68a571
yym68686 <yym68686@outlook.com>
2022-12-07 00:26:09 +0800

    update

--- bot.py
+++ bot.py
@@ -34,9 +34,9 @@ def reset(update, context):
 def process_message(update, context):
     chat_text = update.message.text
     print("1", chat_text)
-    if chat_text.startswith("贴贴"):
+    if chat_text.startswith("javis"):
         chat_id = update.message.chat_id
-        chat_text = chat_text.split("贴贴")[0].strip()
+        chat_text = chat_text.split("javis")[0].strip()
         print("2", chat_text)
         try:
             response_msg = getresult(chat_text)
