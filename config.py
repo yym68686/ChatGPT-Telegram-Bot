bf761e9f7c5388f07fb20b88d60d83ca81fc8336
yym68686 <yym68686@outlook.com>
2022-12-06 10:42:19 +0800

    first

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 admin = 917527833
-# MODE = os.getenv("MODE")
-# URL = os.environ["URL"]
-# BOT_TOKEN = os.environ["BOT_TOKEN"]
-# PORT = int(os.environ.get('PORT', '8080'))
+MODE = os.getenv("MODE")
+URL = os.environ["URL"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+PORT = int(os.environ.get('PORT', '8080'))
 config = {
     "Authorization": "",
     "session_token": os.environ["session_token"]
