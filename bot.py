8f4fe52982763903e27206a78c7ce9968bbb7b58
yym68686 <yym68686@outlook.com>
2022-12-07 00:13:01 +0800

    update

--- bot.py
+++ bot.py
@@ -36,6 +36,7 @@ def process_message(update, context):
     if chat_text.startswith("贴贴"):
         chat_id = update.message.chat_id
         chat_text = chat_text.split("贴贴")[0].strip()
+        print(chat_text)
         try:
             response_msg = getresult(chat_text)
         except Exception:
