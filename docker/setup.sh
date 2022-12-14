3deeeca420f8846a3fedc666e9a20e3e73c8faa9
yym68686 <yym68686@outlook.com>
2022-12-14 19:11:14 +0800

    feature: playwright

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,6 +4,7 @@ echo "code downloaded..." >> /home/log 2>&1
 cd ChatGPT-Telegram-Bot
 pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt > /dev/null
 echo "python env downloaded..." >> /home/log 2>&1
+python -m playwright install --with-deps
 touch /home/log
 nohup python -u /home/ChatGPT-Telegram-Bot/webhook.py >> /home/log 2>&1 &
 echo "web is starting..." >> /home/log 2>&1
