b4a0307a7b63c65ff490a57c8356378c4ce1bf06
yym68686 <yym68686@outlook.com>
2023-02-17 17:51:45 +0800

    fixed bug: telgram bot repeat send POST

--- webhook.py
+++ webhook.py
@@ -2,6 +2,7 @@ import telegram
 from bot import setup
 from urllib import parse
 from waitress import serve
+from threading import Thread
 from flask import Flask, request, jsonify
 from config import BOT_TOKEN, WEB_HOOK, PORT
 
@@ -18,7 +19,9 @@ def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     print("Telegram Bot API send a POST to flask, and now flask send to bot.py.")
     update = telegram.Update.de_json(request.get_json(force=True), updater.bot)
-    dispatcher.process_update(update)
+    thread = Thread(target=dispatcher.process_update, args=(update,))
+    thread.start()
+    # dispatcher.process_update(update)
     return jsonify({'status': 'success', 'message': 'Received message successfully.'})
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
