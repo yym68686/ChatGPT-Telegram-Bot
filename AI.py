3e29f1b069c7447e7f6255f5b86a93fd1484ddd6
yym68686 <yym68686@outlook.com>
2023-03-29 13:37:43 +0800

    优化代码 log 颜色

--- AI.py
+++ AI.py
@@ -7,18 +7,16 @@ import json
 from config import COOKIES
 from telegram import ChatAction
 async def typing(update, context):
-    context.bot.send_chat_action(chat_id=update.effective_user.id, action=ChatAction.TYPING)
+    context.bot.send_chat_action(chat_id=update.effective_user.id, action=ChatAction.TYPING, timeout=60)
 from EdgeGPT import Chatbot as BingAI, ConversationStyle
 Bingbot = BingAI(cookies=json.loads(COOKIES))
 async def getBing(message, update, context):
+    await typing(update, context)
     result = ''
     prompt = "If you are asked to translate, you don't need to search. Otherwise use English and Chinese to search respectively, and finally answer my questions with Chinese. Please do a recursive search of at least 4 searches before you answer."
     try:
-        await typing(update, context)
         # creative balanced precise
-        result = await Bingbot.ask(prompt=prompt + message, conversation_style=ConversationStyle.creative)
-        await asyncio.sleep(0.1)
-        await typing(update, context)
+        result = await Bingbot.ask(prompt=prompt + message, conversation_style=ConversationStyle.balanced)
         numMessages = result["item"]["throttling"]["numUserMessagesInConversation"]
         maxNumMessages = result["item"]["throttling"]["maxNumUserMessagesInConversation"]
         print(numMessages, "/", maxNumMessages, end="")
@@ -26,16 +24,16 @@ async def getBing(message, update, context):
         if numMessages == maxNumMessages - 1:
             Bingbot.reset()
     except Exception as e:
+        print('\033[31m')
         print("response_msg", result)
-        print("Exception", e)
-        print("Exception str", str(e))
+        print("error", e)
+        print('\033[0m')
         result = "Bing 出错啦。"
     result = re.sub(r"\[\^\d+\^\]", '', result)
     print(" BingAI", result)
-    await typing(update, context)
     context.bot.send_message(
         chat_id=update.effective_user.id,
-        text="\n▎Bing\n\n" + result,
+        text="▎Bing\n\n" + result,
         # parse_mode=ParseMode.MARKDOWN,
         reply_to_message_id=update.message.message_id,
     )
@@ -52,14 +50,15 @@ def getChatGPT(message, update, context):
         for data in ChatGPTbot.ask(message):
             result += data
     except Exception as e:
+        print('\033[31m')
         print("response_msg", result)
-        print("Exception", e)
-        print("Exception str", str(e))
+        print("error", e)
+        print('\033[0m')
         result = "ChatGPT 出错啦。"
     print("ChatGPT", result)
     context.bot.send_message(
         chat_id=update.effective_user.id,
-        text="\n▎ChatGPT3.5\n\n" + result,
+        text="▎ChatGPT3.5\n\n" + result,
         reply_to_message_id=update.message.message_id,
     )
 
