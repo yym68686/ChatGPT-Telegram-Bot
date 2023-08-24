d73714171a0d21136db58131bddd49cbb6e45c04
yym68686 <yym68686@outlook.com>
2023-08-24 15:33:26 +0800

    fixed bug: local variable 'api' referenced before assignment

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
+import re
 import os
 import logging
 from md2tgmd import escape
 from runasync import run_async
 from telegram import BotCommand
+from revChatGPT.V3 import Chatbot as GPT
 from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, filters
 from config import BOT_TOKEN, WEB_HOOK, NICK, API, API4, PASS_HISTORY
 from telegram.constants import ChatAction
-from AI import getChatGPT, reset_chat, ChatGPTbot, ChatGPT4bot, Claude2bot, api, api4
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
-async def start(update, context): # 当用户输入/start时，返回文本
-    user = update.effective_user
-    message = (
-        "我是人见人爱的 ChatGPT~\n\n"
-        "欢迎访问 https://github.com/yym68686/ChatGPT-Telegram-Bot 查看源码\n\n"
-        "有 bug 可以联系 @yym68686"
-    )
-    await update.message.reply_html(rf"Hi {user.mention_html()} ! I am an Assistant, a large language model trained by OpenAI. I will do my best to help answer your questions.",)
-    await update.message.reply_text(escape(message), parse_mode='MarkdownV2')
+if API:
+    ChatGPTbot = GPT(api_key=f"{API}")
+    Claude2bot = GPT(api_key=f"{API}", engine="claude-2-web")
+if API4:
+    ChatGPT4bot = GPT(api_key=f"{API4}", engine="gpt-4-0613")
 
 botNick = NICK.lower() if NICK else None
 botNicKLength = len(botNick) if botNick else 0
@@ -34,7 +31,7 @@ async def command_bot(update, context, language="simplified chinese", prompt=tra
         if prompt:
             prompt = prompt.format(language)
             message = prompt + message
-        if (api or api4) and message:
+        if (API or API4) and message:
             await context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
             await getChatGPT(title, robot, message, update, context)
     else:
@@ -57,6 +54,73 @@ async def info(update, context):
         f"**PASS_HISTORY:** `{PASS_HISTORY}`"
     )
     await context.bot.send_message(chat_id=update.message.chat_id, text=escape(message), parse_mode='MarkdownV2')
+
+async def reset_chat(update, context):
+    if API:
+        ChatGPTbot.reset(convo_id=str(update.message.chat_id), system_prompt=systemprompt)
+    if API4:
+        ChatGPT4bot.reset(convo_id=str(update.message.chat_id), system_prompt=systemprompt)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="重置成功！",
+    )
+
+systemprompt = "You are ChatGPT, a large language model trained by OpenAI. Knowledge cutoff: 2021-09. Current date: [ Current Date ]"
+async def getChatGPT(title, robot, message, update, context):
+    result = title
+    text = message
+    modifytime = 0
+    lastresult = ''
+    message = await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="思考中💭",
+        parse_mode='MarkdownV2',
+        reply_to_message_id=update.message.message_id,
+    )
+    messageid = message.message_id
+    try:
+        for data in robot.ask_stream(text, convo_id=str(update.message.chat_id), pass_history=PASS_HISTORY):
+            if data[0] == ' ': # claude-2-web bug fix
+                data = data[1:]
+            result = result + data
+            tmpresult = result
+            modifytime = modifytime + 1
+            if re.sub(r"```", '', result).count("`") % 2 != 0:
+                tmpresult = result + "`"
+            if result.count("```") % 2 != 0:
+                tmpresult = result + "\n```"
+            if modifytime % 12 == 0 and lastresult != tmpresult:
+                if title == 'claude2':
+                    tmpresult = re.sub(r",", '，', tmpresult)
+                await context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=messageid, text=escape(tmpresult), parse_mode='MarkdownV2')
+                lastresult = tmpresult
+    except Exception as e:
+        print('\033[31m')
+        print("response_msg", result)
+        print("error", e)
+        print('\033[0m')
+        if API:
+            robot.reset(convo_id=str(update.message.chat_id), system_prompt=systemprompt)
+        if "You exceeded your current quota, please check your plan and billing details." in str(e):
+            print("OpenAI api 已过期！")
+            await context.bot.delete_message(chat_id=update.message.chat_id, message_id=messageid)
+            messageid = ''
+            API = ''
+        result += f"`出错啦！{e}`"
+    print(result)
+    if lastresult != result and messageid:
+        await context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=messageid, text=escape(result), parse_mode='MarkdownV2')
+
+
+async def start(update, context): # 当用户输入/start时，返回文本
+    user = update.effective_user
+    message = (
+        "我是人见人爱的 ChatGPT~\n\n"
+        "欢迎访问 https://github.com/yym68686/ChatGPT-Telegram-Bot 查看源码\n\n"
+        "有 bug 可以联系 @yym68686"
+    )
+    await update.message.reply_html(rf"Hi {user.mention_html()} ! I am an Assistant, a large language model trained by OpenAI. I will do my best to help answer your questions.",)
+    await update.message.reply_text(escape(message), parse_mode='MarkdownV2')
 
 async def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
