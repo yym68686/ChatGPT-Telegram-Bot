d89f231e611a9542c979ced49121d6f209a4cc57
yym68686 <yym68686@outlook.com>
2023-08-31 23:07:19 +0800

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 git clone --depth 1 -b main https://github.com/yym68686/ChatGPT-Telegram-Bot.git > /dev/null
 echo "code downloaded..." >> /home/log 2>&1
 cd ChatGPT-Telegram-Bot
-pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt --index-url https://pypi.python.org/simple/ > /dev/null
+pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt > /dev/null
 echo "python env downloaded..." >> /home/log 2>&1
 touch /home/log
 nohup python -u /home/ChatGPT-Telegram-Bot/main.py >> /home/log 2>&1 &
