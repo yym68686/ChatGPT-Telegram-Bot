79c746217bf19a4507e4f499e04f5b4fd8444081
yym68686 <yym68686@outlook.com>
2023-09-01 20:36:21 +0800

    update Dockerfile

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 set -eu
 git clone --depth 1 -b main https://github.com/yym68686/ChatGPT-Telegram-Bot.git
 cd ChatGPT-Telegram-Bot
+pip install --upgrade pip
 pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt
 python -u /home/ChatGPT-Telegram-Bot/main.py
