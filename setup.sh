7033c842164f2ce3cc7b4cff04dcbb668d94adfc
yym68686 <yym68686@outlook.com>
2023-09-01 22:07:50 +0800

    fixed bug: search

--- setup.sh
+++ setup.sh
@@ -2,7 +2,4 @@
 set -eu
 rm -rf ChatGPT-Telegram-Bot/
 git clone --depth 1 -b main https://github.com/yym68686/ChatGPT-Telegram-Bot.git
-cd ChatGPT-Telegram-Bot
-pip install --upgrade pip
-pip install -r /home/ChatGPT-Telegram-Bot/requirements.txt
 python -u /home/ChatGPT-Telegram-Bot/main.py
