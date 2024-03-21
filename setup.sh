eb619c236f989845a3065818cf95e65dd0588671
yym68686 <yym68686@outlook.com>
2024-03-21 14:56:43 +0800

    🐛 Bug: Automatically upgrade md2tgmd version in the Docker startup script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 set -eu
 rm -rf /home/ChatGPT-Telegram-Bot
 git clone --depth 1 -b main https://github.com/yym68686/ChatGPT-Telegram-Bot.git
+pip install -U md2tgmd
 python -u /home/ChatGPT-Telegram-Bot/bot.py
---
+++ test/test_tgmd.py
@@ -0,0 +1,4 @@
+import md2tgmd
+a = "### `probs.scatter_(1, ind`"
+# a = "`🤖️ gpt-4-0125-preview`\n\n这段代码是一个关于如何在PyTorch中实现自回归模型生成功能的示例。其中包含了一个`top_k`函数和一个`AutoregressiveWrapper`类。首先，我会解释`top_k`函数中的`probs.scatter_(1, ind, val)`是如何工作的，然后再对整个代码进行概括说明。\n\n### `probs.scatter_(1, ind`"
+print(md2tgmd.escape(a))
