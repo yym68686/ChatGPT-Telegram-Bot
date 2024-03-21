e2573cf2e9056441ee589ea95839a89c1d2c8580
yym68686 <yym68686@outlook.com>
2024-03-21 14:59:44 +0800

    🐛 Bug: Fix md2tgmd version

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 set -eu
 rm -rf /home/ChatGPT-Telegram-Bot
 git clone --depth 1 -b main https://github.com/yym68686/ChatGPT-Telegram-Bot.git
-pip install -U md2tgmd
 python -u /home/ChatGPT-Telegram-Bot/bot.py
