0f131324fe5bcc15fe2300e162d589b822e63e81
yym68686 <yym68686@outlook.com>
2024-04-17 16:35:42 +0800

    🐛 Bug: Fixed bug: Disable Gemini safety content restrictions.

---
+++ test/test_ollama.py
@@ -0,0 +1,52 @@
+import os
+from rich.console import Console
+from rich.markdown import Markdown
+import json
+import requests
+
+def query_ollama(prompt, model):
+    # 设置请求的URL和数据
+    url = 'http://localhost:11434/api/generate'
+    data = {
+        "model": model,
+        "prompt": prompt,
+        "stream": True,
+    }
+
+    response = requests.Session().post(
+        url,
+        json=data,
+        stream=True,
+    )
+    full_response: str = ""
+    for line in response.iter_lines():
+        if not line or line.decode("utf-8")[:6] == "event:" or line.decode("utf-8") == "data: {}":
+            continue
+        line = line.decode("utf-8")
+        # print(line)
+        resp: dict = json.loads(line)
+        content = resp.get("response")
+        if not content:
+            continue
+        full_response += content
+        yield content
+
+if __name__ == "__main__":
+    console = Console()
+    # model = 'llama2'
+    # model = 'mistral'
+    model = 'codeqwen:7b-chat'
+    # model = 'phi'
+
+    # 查询答案
+    prompt = r'''
+
+
+
+'''
+    answer = ""
+    for result in query_ollama(prompt, model):
+        os.system("clear")
+        answer += result
+        md = Markdown(answer)
+        console.print(md, no_wrap=False)
--- utils/chatgpt2api.py
+++ utils/chatgpt2api.py
@@ -1377,6 +1377,24 @@ class gemini_bot:
                 "role": "user",
                 "content": prompt
             }],
+            "safetySettings": [
+                {
+                    "category": "HARM_CATEGORY_HARASSMENT",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_HATE_SPEECH",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                    "threshold": "BLOCK_NONE"
+                }
+            ],
         }
         print(json.dumps(json_post, indent=4, ensure_ascii=False))
 
