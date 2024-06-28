8b28ae37e0316a7d5bc09a65f0b807f9af41a728
yym68686 <yym68686@outlook.com>
2024-06-29 03:47:02 +0800

    ✨ Feature: Support Gemini model to upload images.
    💻 Code: 1. Upgrade ModelMerge version to 0.8.0
    
    2. Optimize the display of log images in base64 encoding, prevent the complete output of base64 encoding.

--- bot.py
+++ bot.py
@@ -230,9 +230,10 @@ async def command_bot(update, context, language=None, prompt=translator_prompt, 
             if Users.get_config(convo_id, "TITLE"):
                 title = f"`🤖️ {engine}`\n\n"
 
-            message = get_image_message(image_url, [{"type": "text", "text": message}], engine)
             if "gemini" in engine:
-                message = message[0]["text"]
+                message = get_image_message(image_url, [{"text": message}], engine)
+            else:
+                message = get_image_message(image_url, [{"type": "text", "text": message}], engine)
             await getChatGPT(update, context, title, robot, message, chatid, messageid, convo_id, message_thread_id, pass_history)
     else:
         message = await context.bot.send_message(
